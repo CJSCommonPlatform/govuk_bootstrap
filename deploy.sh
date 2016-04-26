8ed86120c247f0fbd0d7f546a630fa0abfa03a75
Vygintas Ranonis <vygintas.ranonis@gmail.com>
2016-04-26 13:33:13 +0100

    disabling gh-pages deploy, removing non-dev dependencies from package.json

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@ set -ev
 # BRANCH_NAME=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
 
 
-#if [ "$TRAVIS_TAG" != "" ]; then
+if [ "$TRAVIS_TAG" != "" ]; then
 
   cd docs/app
   git init
 
   # inside this git repo create Travis user
-  git config user.name "vygis"
-  git config user.email "vygintas.ranonis@gmail.com"
+  git config user.name "Travis CI"
+  git config user.email "james@jamesbirrellgray.com"
 
   # The first and only commit to this new Git repo contains all the
   # files present with the commit message "Deploy to GitHub Pages".
@@ -21,5 +21,14 @@ set -ev
   # repo's gh-pages branch. (All previous history on the gh-pages branch
   # will be lost, since we are overwriting it.) We redirect any output to
   # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
-#fi
+
+
+  #git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+  #pushing has been disabled due to issues with GH_TOKEN
+  #in the meanwhile, push manually via <yourusername>@github.com/CJSCommonPlatform/govuk_bootstrap.git
+  #
+fi
+
+
+# GH_TOKEN=nS5UgO*NUR03 GH_REF=github.com/CJSCommonPlatform/govuk_bootstrap.git ./deploy.sh
+# git push --force --quiet "https://vygis@github.com/CJSCommonPlatform/govuk_bootstrap.git" master:gh-pages > /dev/null 2>&1
